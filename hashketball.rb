a18a00f0edbab44a1fe3929a8b416f8801e24930
Mariola Pasko <paskomariola@gmail.com>
2019-12-05 01:45:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -264,7 +264,7 @@ def player_with_longest_name
   name_array.sort_by{|name| name.length}.last
 end
  
- def long_name_steals_a_ton
+ def long_name_steals_a_ton?
   steals = []
   game_hash.each do |location, team|
     players = team[:players]
